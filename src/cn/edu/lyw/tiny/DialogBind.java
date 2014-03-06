4d4c058b2007bce85ca04f271d1afd5ec4520817
JohnsonLow <272462809@qq.com>
2014-03-06 13:37:20 +0800

    测试提交

---
+++ src/cn/edu/lyw/tiny/DialogBind.java
@@ -0,0 +1,70 @@
+package cn.edu.lyw.tiny;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import cn.edu.lyw.tiny.util.ToastUtil;
+
+/**
+ * 绑定用户的对话框
+ * @version 1.0
+ * 
+ */
+public class DialogBind extends Activity {
+
+	/** 退出界面的布局 */
+	private LinearLayout ll_exit_layout;
+
+	/*
+	 * (non-Javadoc)创建界面的回调方法
+	 * 
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dialog_exit);
+		ll_exit_layout = (LinearLayout) findViewById(R.id.ll_exit_layout);
+		ll_exit_layout.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				ToastUtil.showShortToast(getApplicationContext(), "点击窗口外部关闭窗口！");
+			}
+		});
+	}
+
+	/*
+	 * (non-Javadoc)触摸事件的回调方法
+	 * 
+	 * @see android.app.Activity#onTouchEvent(android.view.MotionEvent)
+	 */
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		finish();
+		return true;
+	}
+
+	/**
+	 * 取消退出
+	 * 
+	 * @param v
+	 *            按钮组件
+	 */
+	public void btn_exit_cancle(View v) {
+		finish();
+	}
+
+	/**
+	 * 确定退出
+	 * 
+	 * @param v
+	 *            按钮组件
+	 */
+	public void btn_exit_ok(View v) {
+		finish();
+		MainWeibo.instance.finish();
+	}
+
+}
